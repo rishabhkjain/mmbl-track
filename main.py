7c7cf25ce33a78c1d73e5b29a5c4bc75a32801ef
Rishabh Jain <rishabh@ektovr.com>
2019-06-13 13:19:37 -0400

    add comments and error catching

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from __future__ import division, unicode_literals, print_function  # for compati
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import cv2
 import csv
 import argparse
 mpl.rc('figure',  figsize=(10, 5))
@@ -16,7 +15,7 @@ import pims
 import trackpy as tp
 mpl.use('Agg')
 
-# change the following to %matplotlib notebook for interactive plotting
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
@@ -24,38 +23,40 @@ ap.add_argument("-i", "--input", required=True,
 ap.add_argument("-o", "--output", required=True,
 	help="path to output directory")
 args = vars(ap.parse_args())
-# Optionally, tweak styles.
 
 
-pngStackPath = "media\\" + args["input"] + "\*.png"
-frames = pims.ImageSequence(pngStackPath, as_grey = True)
+pngStackPath = "media\\" + args["input"] + "\*.png" #create path to pngStack
+frames = pims.ImageSequence(pngStackPath, as_grey = True) #import pngstack into trackpy
 startFrame = 0
-endFrame = len(frames)
-trajCont = 10
+endFrame = len(frames) 
+trajCont = 10 #minimum number of times the particle's trajectory needs to be identified
 frameCount = endFrame - startFrame
 
 
 
-
-f = tp.batch(frames[startFrame:endFrame],diameter= 19, invert=True, minmass = 3500)
+#f is a dataframe containing all locations particles were located
+# diameter & minmass need to be adjusted based on sample
+f = tp.batch(frames[startFrame:endFrame],diameter= 19, invert=True, minmass = 3500) 
 t = tp.link_df(f, 5, memory=5)
-compactDict = {}
+compactDict = {} #dictionaries for storing relevant data
 fullDict = {}
 particleLst = set()
-t1 = tp.filter_stubs(t, trajCont)
+t1 = tp.filter_stubs(t, trajCont) #filter out the trajectories that do not exist longer than trajCount
 # Compare the number of particles in the unfiltered and filtered data.
 print('Before:', t['particle'].nunique())
 print('After:', t1['particle'].nunique())
 
 numParticles = t1['particle'].nunique()
+#main loop which stores the data
 for i in range (startFrame,endFrame-1):
-    print(i)
     tv = tp.relate_frames(t1, i, i+1)
     for index, row in tv.iterrows():
+        #extract the data from the dataframe 
         tempDX = row['dx']
         tempDY = row['dy']
         tempDT = row['dr']
         tempDir = row['direction']
+        #replace NaN values with 0
         if pd.isna(tempDT):
             tempDT = 0
         if pd.isna(tempDir):
@@ -91,14 +92,22 @@ for i in range (startFrame,endFrame-1):
             tmpDict = fullDict[index]
             tmpDict[i] = snapshot
             fullDict[index] = tmpDict
+
+#store the data in results
 outputPath = str(args["output"]) +  '\\' + str(args["input"])
-os.mkdir(outputPath)
+try:
+    os.mkdir(outputPath)
+except:
+    print("Overwriting data - directory already exists")
 with open(outputPath + '\\compactResults.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     for key, value in compactDict.items():
         writer.writerow([key, value[0], value[1], value[2], value[3]])
-
-os.mkdir(outputPath + '\\' + "detailedResults")        
+try:
+    os.mkdir(outputPath + '\\' + "detailedResults") 
+except:
+    print("Overwriting data - directory already exists")
+       
 for miniDict in fullDict:
     with open(outputPath + '\\' + "detailedResults" + '\\' + str(miniDict) + '_detailedResult.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
