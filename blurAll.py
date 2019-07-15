2925e892bbb0cee5c1b83c0f0f0691242566f751
rishabhkjain <rishabh@rishabhkjain.com>
2019-07-15 13:29:26 -0400

    update blurring algorithm with better file io

---
+++ blurAll.py
@@ -0,0 +1,26 @@
+
+import os
+import argparse
+import subprocess
+from pathlib import Path
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="directory of videos")
+args = vars(ap.parse_args())
+path = Path(args["input"])
+
+
+
+dirLst = list()
+
+for root,dirs,files in os.walk(path):
+    if not dirs:
+        dirLst.append(root)
+print(dirLst)
+
+
+for item in dirLst:
+    a = 1
+    # subprocess.call(['python', 'blurVideo.py', '--input', path / item])
--- blurVideo.py
+++ blurVideo.py
@@ -1,24 +1,17 @@
 import os
 import argparse
 import subprocess
+from pathlib import Path
 
-
-def findLatest(s, c):
-    strLen = len(s)
-    for i in range(strLen-1, -1, -1):
-        if s[i] == c:
-            return i
-
-    return 0
 
 curLoc = os.getcwd()
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
 	help="directory of videos")
 args = vars(ap.parse_args())
-path = args["input"] 
+path = Path(args["input"])
 
-newPath = path + "_blurred"
+newPath = Path(str(path) + "_blurred")
 try:
 	os.mkdir(newPath)
 except:
@@ -32,7 +25,7 @@ dirLst = os.listdir(path)
 for item in dirLst:
     if item[-1] == "4":
         continue
-    print(path + "\\" + item)
-    subprocess.call(['python', 'blurPhoto.py', '--input', path + "\\" + item, "--output", newPath + "\\" + item])
+    print(path / item)
+    subprocess.call(['python', 'blurPhoto.py', '--input',str( path / item), "--output", str(newPath  / item)])
 
 # print(dirLst)
